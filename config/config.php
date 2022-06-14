9a799d06995f0266834cbb1c0ebcd2b8717e2777
ayrinsupty <ayrinsupty@hotmail.com>
2022-06-15 02:37:48 +0600

    Adding Meta Keywords/Tags to Individual Post

--- config/config.php
+++ config/config.php
@@ -4,3 +4,4 @@ define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "db_blog");
 define("TITLE", "Training With Live Project");
+define("KEYWORDS", "PHP Tutorials, JAVA Tutorials, Oracle Database, C# Tutorials");
